688ae0a6a5bfd4891cff65be5d70d9638ddb396d
Gung Dika <gungdika88@gmail.com>
2024-07-12 12:25:43 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let PopularSlider = new Swiper('.popular-slider', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
